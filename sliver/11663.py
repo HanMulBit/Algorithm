8529773eff929966a346b58cf716bd0ef5d9fc84
HanMulBit <dubuji12@gmail.com>
2025-01-17 18:00:22 +0900

    11663.py

---
+++ sliver/11663.py
@@ -0,0 +1,19 @@
+# 일차원 좌표 점 n개와 선분 m개
+# 각각의 선분 위에 입력으로 주어진 점이 몇 개인지 구하여라
+import bisect
+
+n, m = map(int,input().split())
+dot_list = list(map(int,input().split()))
+
+dot_list.sort()
+
+for _ in range(m):
+    start, end = map(int,input().split())
+    
+    left = bisect.bisect_left(dot_list, start)
+    right = bisect.bisect_right(dot_list, end)
+    
+    result = right - left
+    print(result)
+    
+    
